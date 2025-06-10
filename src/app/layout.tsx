a8db4aa21433b7a2b34814d26c05994069bf7875
Leon Bojanowski <leongaborbojanowski04@gmail.com>
2025-06-10 19:48:29 +0200

    Refactor layout component to improve hydration handling and enforce dark theme in ThemeProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,14 +24,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <ThemeProvider attribute="class" defaultTheme="dark">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          forcedTheme="dark"
+          enableSystem={false}
         >
           {children}
-        </body>
-      </ThemeProvider>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
